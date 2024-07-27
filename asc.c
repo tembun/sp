ada68595db451c3722394e4eed2fbd4f6d501fd9
artembunichev <artembunichev@zohomail.com>
2024-07-27 22:25:22 +0300

    asc: add indentation for 100.handle DEL.

--- asc.c
+++ asc.c
@@ -7,9 +7,11 @@ char s[32][4]={"NUL","SOH","STX","ETX","EOT","ENQ",
 "SLE","DC1","DC2","DC3","DC4","NAK","SYN","ETB",
 "CAN","EM ","SUB","ESC","FS ","GS ","RS ","US "};
 while(1){
+if(c<100)dprintf(1," ");
 if(c<10)dprintf(1," ");
+if(c==127){dprintf(1,"127: DEL\n");break;}
 if(c<32)dprintf(1,"%d: %s  ",c,s[c]);
 else dprintf(1,"%d: %c  ",c,c);
-if(c>111){dprintf(1,"\n");if(c==127)break;c=(c%16)+1;}
+if(c>111){dprintf(1,"\n");c=(c%16)+1;}
 else c+=16;}
 return 0;}
