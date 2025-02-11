7669c5060c654fbdcba1ed6dac10e7ab2bb93daa
artembunichev <artembunichev@zohomail.com>
2025-02-11 15:28:31 +0300

    len: don't count newline characters in length.

--- len.c
+++ len.c
@@ -8,10 +8,17 @@ int l;/*input string length (number).*/
 int ldc;/*length digit count.*/
 int j;/*iterator for filling result string (initially copy of ldc variable).*/
 int z;/*temporary variable for getting length digit count (copy of l variable).*/
+int i;/*input buffer iterator.*/
 char* rs;/*resulting string.*/
 l=0;
 ldc=0;
-while((rb=read(0,&buf,4096))>0)l+=rb;
+while((rb=read(0,&buf,4096))>0) {
+	i=0;
+	while (i<rb) {
+		if (buf[i] != '\n' && buf[i] != '\r') l+=1;
+		++i;
+	}
+}
 if(rb==-1){write(2,"can't read stdin\n",17);return 1;}
 /*getting l variable number value decimal digit count*/
 z=l;
