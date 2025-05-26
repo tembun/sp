a872a12635c874cd8c1367d7d7ee88dc260accf4
Artem Bunichev <tembun@bk.ru>
2025-05-26 13:28:36 +0300

    fl: Style

--- fl.c
+++ fl.c
@@ -1,48 +1,48 @@
 /*
-fl -- create a first hard link to a file(s).
+ * fl -- create a first hard link to a file(s).
+ */
 
-I consider it as an utility
-for creating files(actually, creating
-a first hard link to them), instead
-of touch(1), which seems to be for
-setting file timestamps.
-*/
+#include <sys/stat.h>
 
-#include<fcntl.h>
-#include<sys/stat.h>
-#include<unistd.h>
-#include<stdio.h>
+#include <err.h>
+#include <errno.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
 
-int main(int argc, char** argv){
-int i;
-struct stat st;
-
-if(argc < 2){
-	write(2,"[fl]: specify at least one name for a file to create a link for.\n",65);
-	return 1;
-}
-
-i=1;
-while(i<argc){
-	if(!stat(argv[i],&st)){
-		dprintf(2,"[fl]: %s exists already.\n", argv[i]);
-		goto next;
+int
+main(int argc, char** argv)
+{
+	int i;
+	struct stat st;
+	
+	if (argc < 2) {
+		errx(1, "Usage: fl file ...");
+		return 1;
 	}
-
-	/*
-	DEFFILEMODE is for default file mode, which
-	is 666 (but bear in mind, it'll be restricted
-	by umask(2)).
-	For DEFFILEMODE, see /usr/include/sys/stat.h.
-	For file permission defines, see stat(2).
-	*/
-	if(open(argv[i],O_CREAT|O_WRONLY,DEFFILEMODE) == -1) {
-		dprintf(2,"[fl]: can't create %s.\n",argv[i]);
+	
+	i = 1;
+	while(i < argc) {
+		if (stat(argv[i],&st) == 0) {
+			dprintf(STDERR_FILENO,
+			    "[fl]: %s already exists\n", argv[i]);
+			goto next;
+		}
+	
+		/*
+		 * DEFFILEMODE is for default file mode, which is 666 (but bear
+		 * in mind, it'll be restricted by umask(2)).
+		 *
+		 * For DEFFILEMODE, see /usr/include/sys/stat.h.
+		 * For file permission defines, see stat(2).
+		 */
+		if (open(argv[i], O_CREAT | O_WRONLY, DEFFILEMODE) == -1)
+			dprintf(STDERR_FILENO, "[fl]: Can't create %s: %s\n",
+			    argv[i], strerror(errno));
+next:
+		i++;
 	}
-
-	next:
-	++i;
-}
-
-return 0;
+	
+	return 0;
 }
