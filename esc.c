9aad634062944ea921206768121e00ce1bd6947a
Artem Bunichev <tembun@bk.ru>
2025-07-06 23:27:39 +0300

    esc: style(9)
    Yes, now it doesn't fit into 32 lines.
    Yes, it now has three #includes instead of two.
    Yes, for instead of while.
    
    AND I DO FUCKING LIKE IT!!!

--- esc.c
+++ esc.c
@@ -1,58 +1,89 @@
-/*esc-escape(\n->\\n and so on) string from stdin and write it to stdout.*/
-#include<unistd.h>
-#include<stdlib.h>
+/*
+ * esc -- escape input.
+ *
+ * Escaping means displaying newlines as `\n', tab characters as `\t' and so on.
+ */
+
+#include <err.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#define BUF_SZ 4096
+
 int
-main(){
-char bf[4096];//input buffer
-ssize_t rb=0;//how many bytes were read from stdin
-int i;//input buffer iteration index
-char* r=0;//resulting escaped string
-int j=0;//current index of resulting string
-int rl=0;//length of resulting string buffer. need it for realloc
-char c;//charcter to be appended to result
-/*read stdin by portions*/
-while((rb=read(0,&bf,4096))>0){
-/*expand resulting string buffer by the length of current
-portion AND reserve some arbitrary amount of extra space for escaped
-characters. actually, it's a 1.5*rb.*/
-r=realloc(r,rl+=(rb+(rb>>1)));
-if(!r){write(2,"can't realloc.\n",15);return 1;}
-i=0;//every time start buffer iteration from its begining
-/*iterate over the input buffer*/
-while(i<rb){
-/*we're going to insert two(or one, but two is maximum) characters and we need to
-ensure we've got enough space for them*/
-if((j+1)>rl){//if there is no enough space
-/*reserve 2 bytes for character we're going to insert right now
-plus allocate new space for the following part of input and also some
-extra space for escaped chars that may occur in that following part ofinput too*/
-r=realloc(r,rl+=(2+((rb-i-1)>>1)));
-if(!r){write(2,"can't realloc.\n",15);return 1;}
+main()
+{
+	char bf[BUF_SZ]; /* Input buffer. */
+	ssize_t rb; /* How many bytes we've read from stdin. */
+	int i; /* Input buffer iterator. */
+	char* r; /* Resulting escaped string. */
+	int j; /* Current index of `r'. */
+	int rl; /* Length of `r'. */
+	char c; /* Character to be appended to `r'. */
+	
+	rb = 0;
+	r = NULL;
+	j = 0;
+	rl = 0;
+	
+	/* Read stdin by portions. */
+	while ((rb = read(STDIN_FILENO, &bf, BUF_SZ)) != 0) {
+		if (rb < 0)
+			err(1, "read()");
+		
+		/*
+		 * Expand resulting string buffer by the length of current
+		 * portion and reserve some arbitrary amount of extra space for
+		 * escaped characters.
+		 */
+		if ((r = realloc(r, rl += (rb + rb / 2))) == NULL)
+			err(1, "realloc()");
+		
+		/* Iterate over the input buffer. */
+		for (i = 0; i < rb; i++) {
+			/*
+			 * We're going to insert two(or one, but two is maximum)
+			 * characters and we need to make sure we've got enough
+			 * space for them.
+			 */
+			if (j + 1 > rl) {
+				/*
+				 * Reserve 2 bytes for character we're going to
+				 * insert right now plus allocate new space for
+				 * the following part of input and also some
+				 * extra space for escaped characters that may
+				 * occur in that following part.
+				 */
+				if ((r = realloc(r, rl += (rb - i + 1) / 2)) ==
+				    NULL)
+					err(1, "realloc()");
+			}
+			
+			/* By default, insert the character as is. */
+			c = bf[i];
+			switch (bf[i]) {
+			case '\n':
+				c = 'n';
+				goto insert_slash;
+			case '\t':
+				c = 't';
+				goto insert_slash;
+			case '\\':
+			case '\'':
+			case '"':
+			case '`':
+			insert_slash:
+				r[j++] = '\\';
+				/* FALLTHROUGH. */
+			default:
+				r[j++] = c;
+			}
+		}
+	}
+	
+	if (write(STDOUT_FILENO, r, j) < 0)
+		err(1, "write()");
+	free(r);
+	
+	return 0;
 }
-c=bf[i];//by default we're goint to insert character as it is.
-switch(bf[i]){
-/*in case of control characters, we need to manually specify characters that are next
-to the \ character*/
-case '\n':c='n';goto is;
-case '\t':c='t';goto is;
-/*in case of quotes and slash symbol we also need to prepend them with \ and write
-them as they are.*/
-case '\\':
-case '\'':
-case '"':
-case '`':/*insert slash label*/is:{
-r[j++]='\\';//insert slash character
-}
-default:{
-r[j++]=c;//insert character we've specified before
-++i;//continue input buffer iteration
-}
-}
-}
-}
-/*here we might leave the read loop not because we've reached the end,
-but because of the error occured*/
-if(rb==-1){write(2,"can't read stdin.\n",18);return 1;}
-write(1,r,j);//write resulting string to stdout
-free(r);//free resulting string memory
-return 0;}
